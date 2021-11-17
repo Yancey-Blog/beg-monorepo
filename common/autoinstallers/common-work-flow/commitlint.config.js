0c59d95c4d77ca8289ec79101f1b413db05b7238
YanceyOfficial <yanceyofficial@gmail.com>
2021-11-17 13:59:23 +0800

    chore(*): update dependencies

--- common/autoinstallers/common-work-flow/commitlint.config.js
+++ common/autoinstallers/common-work-flow/commitlint.config.js
@@ -2,20 +2,21 @@ const rushLib = require('@microsoft/rush-lib')
 const rushConfiguration = rushLib.RushConfiguration.loadFromDefaultLocation()
 
 const packageNames = []
-const packageDirNames = []
+// const packageDirNames = []
 
 rushConfiguration.projects.forEach((project) => {
   packageNames.push(project.packageName)
-  const temp = project.projectFolder.split('/')
-  const dirName = temp[temp.length - 1]
-  packageDirNames.push(dirName)
+  // const temp = project.projectFolder.split('/')
+  // const dirName = temp[temp.length - 1]
+  // packageDirNames.push(dirName)
 })
 
-const allScope = ['all', ...packageDirNames, ...packageNames]
+// const allScope = ['*', ...packageDirNames, ...packageNames]
+const allScope = ['*', ...packageNames]
 
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'scope-enum': [2, 'always', allScope],
-  },
+    'scope-enum': [2, 'always', allScope]
+  }
 }
