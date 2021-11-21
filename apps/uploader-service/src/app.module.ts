047801bb19abc802ab219d2078795ec1c2316e05
YanceyOfficial <yanceyofficial@gmail.com>
2021-11-21 17:57:45 +0800

    feat: delete useless files

--- apps/uploader-service/src/app.module.ts
+++ apps/uploader-service/src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common'
-import { ConfigsModule } from './config/config.module'
 import { UploaderModule } from './uploader/uploader.module'
 import { LoggerModule } from './libs/loggers/logger.module'
 
 @Module({
-  imports: [LoggerModule, ConfigsModule, UploaderModule]
+  imports: [LoggerModule, UploaderModule]
 })
 export class AppModule {}
