dadb4a4a78929956e95b6aae609a5b08df29c69f
YanceyOfficial <yanceyofficial@gmail.com>
2021-11-28 21:21:48 +0800

    feat: update logger

--- apps/blog-service/src/main.ts
+++ apps/blog-service/src/main.ts
@@ -1,19 +1,22 @@
 import { NestFactory } from '@nestjs/core'
 import { NestExpressApplication } from '@nestjs/platform-express'
-import {
-  RoleGuard,
-  AuthGuard,
-  KeycloakConnectModule
-} from 'nest-keycloak-connect'
 import { configLogger } from './shared/logger/logger.config'
 import { configMiddlewares } from './shared/middlewares/middleware.config'
 import { AppModule } from './app.module'
 
 const bootstrap = async () => {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule)
+  const isEnvProduction = process.env.NODE_ENV === 'production'
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+    isEnvProduction
+      ? {
+          logger: false
+        }
+      : {}
+  )
   app.setGlobalPrefix('beg')
   configMiddlewares(app)
-  // configLogger(app)
+  configLogger(app)
   await app.listen(process.env.port || 3002)
 }
 
