761a857aaff6658a67dfc56945937fde62cdcf99
YanceyOfficial <yanceyofficial@gmail.com>
2021-11-29 12:08:08 +0800

    feat: integrate keycloak

--- apps/blog-service/src/shared/middlewares/middleware.config.ts
+++ apps/blog-service/src/shared/middlewares/middleware.config.ts
@@ -1,12 +1,10 @@
 import { INestApplication } from '@nestjs/common'
 import path from 'path'
 import serveFavicon from 'serve-favicon'
-import morgan from 'morgan'
 import helmet from 'helmet'
 
 export const configMiddlewares = (app: INestApplication) => {
   app.use(serveFavicon(path.join(process.cwd(), 'public/favicon.ico')))
-  app.use(morgan('combined'))
   app.use(
     helmet({
       contentSecurityPolicy:
