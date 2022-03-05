c5fa5f5ba453ac9392cbb6f0283512f924895f28
Yancey Leo <yanceyofficial@gmail.com>
2022-03-06 00:58:30 +0800

    fix: fix sentry config

--- apps/blog-web/sentry.server.config.js
+++ apps/blog-web/sentry.server.config.js
@@ -4,10 +4,8 @@
 
 import * as Sentry from '@sentry/nextjs'
 
-const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
-
 Sentry.init({
-  dsn: SENTRY_DSN,
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   tracesSampleRate: 1.0,
   debug: process.env.NODE_ENV !== 'production'
   // Note: if you want to override the automatic release value, do not set a
