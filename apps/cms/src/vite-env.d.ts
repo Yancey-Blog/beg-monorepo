d6c2118ff4421cce74faf8d88db62e344a515ad5
YanceyOfficial <yanceyofficial@gmail.com>
2024-11-30 19:01:11 +0800

    feat: delete useless code

--- apps/cms/src/vite-env.d.ts
+++ apps/cms/src/vite-env.d.ts
@@ -7,7 +7,6 @@ declare namespace NodeJS {
     VITE_ALGOLIA_APPLICATION_ID: string
     VITE_ALGOLIA_ADMIN_API_KEY: string
     VITE_ALGOLIA_SEARCH_INDEX: string
-    PORT: string
     VITE_KEY_CLOAK_REALM: string
     VITE_KEY_CLOAK_URL: string
     VITE_KEY_CLOAK_CLIENT_ID: string
