e940dbf2c2c120e745d5bcf0d76b3e1e8e889f06
YanceyOfficial <yanceyofficial@gmail.com>
2024-11-30 18:48:11 +0800

    ci: update ci

--- apps/backend/src/config/config.module.ts
+++ apps/backend/src/config/config.module.ts
@@ -6,10 +6,7 @@ import { ConfigService } from './config.service'
   providers: [
     {
       provide: ConfigService,
-      useValue: new ConfigService([
-        'env/.env',
-        `env/${process.env.NODE_ENV || 'development'}.env`
-      ])
+      useValue: new ConfigService(['.env'])
     }
   ],
   exports: [ConfigService]
